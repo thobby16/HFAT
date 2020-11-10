5ecae3a6ffca0a81863b10e0425cf5993f7bb45b
Bito165 <fbabalola@sbsc.com>
2020-11-10 09:59:13 +0100

    Seed Fix

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DragDropDirective } from './directives/dragdrop/dragdrop.directive';
+import { AbbPipe } from './pipes/abbriviate/abb.pipe';
+import { TrustedURLPipe } from './pipes/trustedURL/trusted-urlpipe.pipe';
+import { NumberFormatPipe } from './pipes/negativeNumber/negative-number.pipe';
+import { HumanizePipe } from './pipes/humanize/humanize.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DragDropDirective, AbbPipe, TrustedURLPipe, NumberFormatPipe, HumanizePipe],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [DragDropDirective, AbbPipe, TrustedURLPipe, NumberFormatPipe, HumanizePipe]
 })
 export class ServicesModule { }
